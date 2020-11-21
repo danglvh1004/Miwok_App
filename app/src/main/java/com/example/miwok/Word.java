691b78ef76da2fe53f3def7209339d7ada1ced52
Lê Vũ Hải Đăng <56032105+lvhd-jp@users.noreply.github.com>
2020-11-21 08:52:40 +0700

    NA

--- app/src/main/java/com/example/miwok/Word.java
+++ app/src/main/java/com/example/miwok/Word.java
@@ -3,7 +3,8 @@ package com.example.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mImageResourceId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String mDefaultTranslation, String mMiwokTranslation) {
         this.mDefaultTranslation = mDefaultTranslation;
@@ -34,5 +35,10 @@ public class Word {
 
     public int getmImageResourceId() {
         return mImageResourceId;
+    }
+
+    //Return whether or not there is an image for this word.
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/miwok/WordAdapter.java
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -38,8 +38,15 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         //Find the ImageView on the list_item.xml layout with the ID image.
         ImageView imageView = listItemView.findViewById(R.id.image);
-        //Set thw ImageView to the image resource specified in the current Word.
-        imageView.setImageResource(currentWord.getmImageResourceId());
+        if (currentWord.hasImage()) {
+            //Set the ImageView to the image resource specified in the current Word.
+            imageView.setImageResource(currentWord.getmImageResourceId());
+            //Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
 
         //Return the whole list item layout (containing 2 TextViews) so that it can be shown in the ListView.
         return listItemView;
